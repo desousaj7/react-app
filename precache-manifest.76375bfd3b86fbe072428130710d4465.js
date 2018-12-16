0822dcb5553690f714b6d8fb0dd314b21233efb9
desousaj7 <jhonnysousa@alu.ufc.br>
2018-12-16 18:35:44 -0200

    Updates

---
+++ precache-manifest.76375bfd3b86fbe072428130710d4465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactMyPage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06748171c8403b364e4f",
+    "url": "/ReactMyPage/static/js/runtime~main.06748171.js"
+  },
+  {
+    "revision": "2c8f20f13a59c01e5d21",
+    "url": "/ReactMyPage/static/js/main.2c8f20f1.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/ReactMyPage/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "2c8f20f13a59c01e5d21",
+    "url": "/ReactMyPage/static/css/main.be6410a0.chunk.css"
+  },
+  {
+    "revision": "a964b40b74484082c2d8986213e13df6",
+    "url": "/ReactMyPage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.8bf5a958ef3325ddc2c4d07cab63ca47.js"
+  "/ReactMyPage/precache-manifest.76375bfd3b86fbe072428130710d4465.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/ReactMyPage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
